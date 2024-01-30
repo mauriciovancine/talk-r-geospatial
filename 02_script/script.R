935caeb674f90a42c0c456519e7c5b62ac393d12
mauriciovancine <mauricio.vancine@gmail.com>
2024-01-30 17:27:55 -0300

    script and data

--- 02_script/script.R
+++ 02_script/script.R
@@ -1,5 +1,5 @@
 #' ----
-#' theme: análise de dados geoespaciais no r
+#' theme: analise de dados geoespaciais no r
 #' autor: mauricio vancine
 #' data: 2024-03-12
 #' ----
@@ -11,18 +11,22 @@ library(tidyverse)
 library(sf)
 library(stars)
 library(terra)
+library(geodata)
 library(tmap)
+
+# options
+options(scipen = 1e3)
 
 # importar dados ----------------------------------------------------------
 
 # tabela ----
 
 ## importar ----
-pontos <- readr::read_csv("03_data/pontos.csv")
+pontos <- readr::read_csv("03_data/pontos_campinas.csv")
 pontos
 
 plot(pontos)
-plot(pontos[, 2:3])
+plot(pontos[, 2:3], pch = 20, cex = 2)
 
 # vetor ----
 
@@ -30,7 +34,7 @@ plot(pontos[, 2:3])
 
 ### terra ----
 # transformar
-pontos_terra <- terra::vect(as.matrix(pontos[, 2:3]), crs = "+proj=longlat +datum=WGS84")
+pontos_terra <- terra::vect(as.matrix(pontos[, 2:3]), crs = "EPSG:4326")
 pontos_terra
 
 # importar
@@ -88,10 +92,199 @@ mapbiomas_stars
 
 plot(mapbiomas_stars)
 
+## worldclim ----
+### precipitation ----
+precipitation <- geodata::worldclim_tile(var = "prec", 
+                                         lon = pontos$longitude[1], 
+                                         lat = pontos$latitude[1], 
+                                         path = "03_data")
+precipitation
+plot(precipitation)
+
+### temperature ----
+temperature <- geodata::worldclim_tile(var = "tavg", 
+                                       lon = pontos$longitude[1], 
+                                       lat = pontos$latitude[1], 
+                                       path = "03_data")
+temperature
+plot(temperature)
+
+### elevation ----
+elevation <- geodata::elevation_3s(lon = pontos$longitude[1], 
+                                   lat = pontos$latitude[1], 
+                                   path = "03_data")
+elevation
+plot(elevation)
+
 # reprojetar --------------------------------------------------------------
 
 ## vetor ----
 
+### terra ----
+pontos_terra_utm <- terra::project(x = pontos_terra, y = "EPSG:32723")
+pontos_terra_utm
+plot(pontos_terra_utm)
 
+### sf ----
+pontos_sf_utm <- sf::st_transform(pontos_sf, crs = 32723)
+pontos_sf_utm
+plot(pontos_sf_utm$geometry, pch = 20, axes = TRUE)
+
+## raster ----
+res(mapbiomas_terra)[1]*3600*30
+
+mapbiomas_terra_utm <- terra::project(x = mapbiomas_terra, y = "EPSG:32723", method = "near", res = 10)
+mapbiomas_terra_utm
+plot(mapbiomas_terra_utm)
+
+# operacoes -------------------------------------------------
+
+## extrair valores para pontos ----
+pontos_sf_elev <- pontos_sf %>% 
+    dplyr::mutate(elev = terra::extract(elevation, pontos_terra, ID = FALSE)[, 1])
+pontos_sf_elev
+
+pontos_terra_prep <- terra::extract(precipitation, pontos_terra, ID = FALSE)
+colnames(pontos_terra_prep) <- sub("tile_41_wc2.1_30s_", "", colnames(pontos_terra_prep))
+pontos_terra_prep
+
+pontos_terra_temp <- terra::extract(temperature, pontos_terra, ID = FALSE)
+colnames(pontos_terra_temp) <- sub("tile_41_wc2.1_30s_", "", colnames(pontos_terra_temp))
+pontos_terra_temp
+
+pontos_sf_elev_prec_temp <- pontos_sf_elev %>% 
+    dplyr::bind_cols(pontos_terra_prep, pontos_terra_temp)
+pontos_sf_elev_prec_temp
+
+## media dos rasters
+precipitation_annual <- terra::app(precipitation, sum)
+precipitation_annual
+plot(precipitation_annual)
+
+temperature_mean <- terra::mean(temperature)
+temperature_mean
+plot(temperature_mean)
+
+pontos_sf_elev_prec_temp <- pontos_sf_elev %>% 
+    dplyr::mutate(prec = terra::extract(precipitation_annual, pontos_terra, ID = FALSE)[, 1],
+                  temp = terra::extract(temperature_mean, pontos_terra, ID = FALSE)[, 1],)
+pontos_sf_elev_prec_temp
+
+## extensao e mascara ----
+mapbiomas_terra_campinas <- terra::crop(mapbiomas_terra, campinas_terra, mask = TRUE)
+elevation_campinas <- terra::crop(elevation, campinas_terra, mask = TRUE)
+precipitation_annual_campinas <- terra::crop(precipitation_annual, campinas_terra, mask = TRUE)
+temperature_mean_campinas <- terra::crop(temperature_mean, campinas_terra, mask = TRUE)
+
+plot(mapbiomas_terra_campinas)
+plot(elevation_campinas)
+plot(precipitation_annual_campinas)
+plot(temperature_mean_campinas)
+
+## reamostragem ----
+mapbiomas_terra_campinas_rea <- terra::resample(mapbiomas_terra_campinas, precipitation_annual_campinas, method = "near")
+mapbiomas_terra_campinas_rea
+plot(mapbiomas_terra_campinas_rea)
+
+elevation_campinas_rea <- terra::resample(elevation_campinas, precipitation_annual_campinas)
+elevation_campinas_rea
+plot(elevation_campinas_rea)
+
+precipitation_annual_campinas_rea <- terra::resample(precipitation_annual_campinas, elevation_campinas)
+precipitation_annual_campinas_rea
+plot(precipitation_annual_campinas_rea)
+
+## buffer ----
+pontos_terra_buffer <- terra::buffer(pontos_terra, width = 1000)
+pontos_terra_buffer
+
+plot(campinas_terra)
+plot(pontos_terra, add = TRUE)
+plot(pontos_terra_buffer, add = TRUE)
+
+## extrair dados para o buffer ----
+pontos_terra_buffer_amb <- terra::zonal(c(elevation_campinas_rea, precipitation_annual_campinas, temperature_mean_campinas),
+                                        pontos_terra_buffer, fun = "mean")
+pontos_terra_buffer_amb
+
+pontos_sf_elev_prec_temp
+
+# maps --------------------------------------------------------------------
+
+## pontos ----
+map_pontos <- tm_shape(campinas_sf) +
+    tm_polygons() +
+    tm_shape(pontos_sf_elev_prec_temp) +
+    tm_bubbles(fill = "elev",
+               fill.scale = tm_scale_continuous(values = "-RdYlGn"),
+               fill.legend = tm_legend(title = "Elevação (m)", 
+                                      position = tm_pos_in("left", "top"),
+                                      reverse = TRUE)) +
+    tm_graticules(lines = FALSE) +
+    tm_compass(position = c("right", "bottom"), size = 3) +
+    tm_scalebar(breaks = c(0, 5, 10), text.size = 1, position = c("right", "bottom"))
+map_pontos
+
+## uso ----
+map_uso <- tm_shape(mapbiomas_terra_campinas) +
+    tm_raster(col.scale = tm_scale_categorical(values = c("#1f8d49", "#7a5900", "#519799",
+                                                          "#edde8e", "#C27BA0", "#ffefc3",
+                                                          "#d4271e", "#db4d4f", "#ffaa5f",
+                                                          "#9c0027", "#2532e4", "#d082de")),
+              col.legend = tm_legend(title = "Classes", 
+                                     position = tm_pos_in("left", "top"))) +
+    tm_shape(campinas_sf) +
+    tm_borders() +
+    tm_graticules(lines = FALSE) +
+    tm_compass(position = c("right", "bottom"), size = 3) +
+    tm_scalebar(breaks = c(0, 5, 10), text.size = 1, position = c("right", "bottom"))
+map_uso
+
+## ambiental ----
+ambiental <- c(elevation_campinas_rea, precipitation_annual_campinas, temperature_mean_campinas)
+ambiental
+plot(ambiental)
+
+map_elev <- tm_shape(elevation_campinas_rea) +
+    tm_raster(col.scale = tm_scale_continuous(values = "-RdYlGn"),
+              col.legend = tm_legend(title = "Elevação (m)", 
+                                     position = tm_pos_in("left", "top"),
+                                     reverse = TRUE)) +
+    tm_shape(campinas_sf) +
+    tm_borders() +
+    tm_graticules(lines = FALSE) +
+    tm_compass(position = c("right", "bottom"), size = 3) +
+    tm_scalebar(breaks = c(0, 5, 10), text.size = 1, position = c("right", "bottom"))
+map_elev
+
+map_prec <- tm_shape(precipitation_annual_campinas) +
+    tm_raster(col.scale = tm_scale_continuous(),
+              col.legend = tm_legend(title = "Precipitação (mm)", 
+                                     position = tm_pos_in("left", "top"),
+                                     reverse = TRUE)) +
+    tm_shape(campinas_sf) +
+    tm_borders() +
+    tm_graticules(lines = FALSE) +
+    tm_compass(position = c("right", "bottom"), size = 3) +
+    tm_scalebar(breaks = c(0, 5, 10), text.size = 1, position = c("right", "bottom"))
+map_prec
+
+map_temp <- tm_shape(temperature_mean_campinas) +
+    tm_raster(col.scale = tm_scale_continuous(values = "-Spectral"),
+              col.legend = tm_legend(title = "Temperatural (ºC)", 
+                                     position = tm_pos_in("left", "top"),
+                                     reverse = TRUE)) +
+    tm_shape(campinas_sf) +
+    tm_borders() +
+    tm_graticules(lines = FALSE) +
+    tm_compass(position = c("right", "bottom"), size = 3) +
+    tm_scalebar(breaks = c(0, 5, 10), text.size = 1, position = c("right", "bottom"))
+map_temp
+
+map <- tmap_arrange(map_uso, map_elev, map_prec, map_temp)
+map
+
+## export
+tmap_save(map, "03_data/map.png", width = 45, height = 30, units = "cm", dpi = 300, asp = FALSE)
 
 # end ---------------------------------------------------------------------
